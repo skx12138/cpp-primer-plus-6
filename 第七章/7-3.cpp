8039bde9a0cf756dfd267d587936abc9beec9ac8
skx12138 <804066634@qq.com>
2023-07-28 20:45:31 +0800

    第七章

--- 第七章/7-3.cpp
+++ 第七章/7-3.cpp
@@ -1,11 +1,7 @@
-/*
-a.编写一个函数，按值传递box结构，并显示每个成员的值。
-b. 编写一个函数，传递box结构的地址， 并将volume成员设置为其他三维长度的乘积。
-c.编写一个使用这两个函数的简单程序。
-*/
 #include <iostream>
 #include <cstring>
 using namespace std;
+
 struct box
 {
     char maker[40];
@@ -16,7 +12,7 @@ struct box
 };
 
 void display(const box myBox);
-void computeVolume(box *mybox, float height, float width, float length, float volume);
+void computeVolume(box *mybox);
 
 int main()
 {
@@ -26,21 +22,22 @@ int main()
     mybox.width = 5.0;
     mybox.length = 8.0;
 
-    computeVolume(&mybox, mybox.height, mybox.width, mybox.length, mybox.volume);
+    computeVolume(&mybox);
     display(mybox);
+
+    return 0;
 }
 
-// 计算体积
-void computeVolume(box *mybox, float height, float width, float length, float volume)
+void computeVolume(box *mybox)
 {
-    mybox->volume = height * width * length;
+    mybox->volume = mybox->height * mybox->width * mybox->length;
 }
 
 void display(const box mybox)
 {
-    cout << "制造商:" << mybox.maker << endl
-         << "高度:" << mybox.height << endl
-         << "宽度:" << mybox.width << endl
-         << "长度:" << mybox.length << endl
-         << "体积:" << mybox.volume;
+    cout << "制造商: " << mybox.maker << endl
+         << "高度: " << mybox.height << endl
+         << "宽度: " << mybox.width << endl
+         << "长度: " << mybox.length << endl
+         << "体积: " << mybox.volume << endl;
 }
